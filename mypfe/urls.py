0fcc293fb1cd9346bf3c21f0886e2b2d608e3a90
emna-malek <62110040+emna-malek@users.noreply.github.com>
2020-03-12 17:40:54 +0100

    Update urls.py

--- mypfe/urls.py
+++ mypfe/urls.py
@@ -1,1 +1,22 @@
 
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('cart/', include('cart.urls', namespace='cart')),
+    path('orders/', include('orders.urls')),
+    path('paypal/', include('paypal.standard.ipn.urls')),
+    path('payment/', include('payment.urls')),
+    path('coupons/', include('coupons.urls')),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
+    path('', include('myapp.urls', namespace='shop')),
+
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
